61f35bb230b66ec7250147dccfd9922ee37a6cc7
kazuki <klklklkl063055@gmail.com>
2021-11-21 15:39:54 +0900

    modify the comment

--- deletedata.php
+++ deletedata.php
@@ -1,21 +1,22 @@
 <?php
+
+// Using database connection file here
+include "db_conn.php"; 
+
+// get module_cd through query string
+$module_cd = $_GET['module_cd']; 
+
+// get student ID through the session
 session_start();
-
-include "db_conn.php"; // Using database connection file here
-
-$module_cd = $_GET['module_cd']; // get id through query string
 $student_id = $_SESSION['student_id'];
 
-$del = mysqli_query($conn,"delete from modules where module_cd = '$module_cd' and student_id = '$student_id'  "); // delete query
+// delete query
+$del = mysqli_query($conn,"delete from modules where module_cd = '$module_cd' and student_id = '$student_id'  "); 
 
-if($del)
-{
+if($del){
     mysqli_close($conn); // Close connection
     header("location:home.php"); // redirects to all records page
     exit;	
-}
-else
-{
-    echo "Error deleting record"; // display error message if not delete
-}
+}else{ echo "Error deleting record"; }
+
 ?>
--- home.php
+++ home.php
@@ -7,8 +7,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['student_id'])) {
 <html>
 <head>
 	<title>HOME</title>
-     <!-- Fonts -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="home.css">
     <script type="text/javascript">
